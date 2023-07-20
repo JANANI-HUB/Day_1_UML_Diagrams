0d9052858ad4688eb940ce8b38ca21e7cf41ef58
JANANI-HUB <113664724+JANANI-HUB@users.noreply.github.com>
2023-07-20 10:05:27 +0530

    Add files via upload

---
+++ Day6/src/Paymentsuccess.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Paymentsuccess() {
+  return (
+    <body>
+  <div class="containerps">
+    <img src="success-icon.png" alt="Success Icon" class="success-icon"/>
+    <h1>Payment Successful</h1>
+    <p>Thank you for your payment. Your transaction has been successfully processed.</p>
+    <a href="#" class="buttonps">Continue Shopping</a>
+  </div>
+</body>
+
+  )
+}
+
+export default Paymentsuccess
